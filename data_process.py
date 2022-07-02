82272ca8faa014af59facd0f046dabc14ef13cec
haiying <hsun30713@gmail.com>
2022-07-02 15:44:41 +0800

    u

--- data_process.py
+++ data_process.py
@@ -69,6 +69,16 @@ class Processor:
                 if not line: # meaning the end of the training_data file?
                     x_train.append(line_x)
                     y_train.append(line_y)
+                    if len(line_x) > config.max_len:
+                        # 直接按最大长度切分
+                        sub_word_list = get_sub_list(line_x, config.max_len - 5, config.sep_word)
+                        sub_label_list = get_sub_list(line_y, config.max_len - 5, config.sep_label)
+                        x_train.extend(sub_word_list)
+                        y_train.extend(sub_label_list)
+                        sep_num += 1
+                    else:
+                        x_train.append(line_x)
+                        y_train.append(line_y)
                     line_x = []
                     line_y = []
                     continue
@@ -83,45 +93,16 @@ class Processor:
         print("y_train[0]: ", y_train[0])
         logging.info("-------- {} data process DONE!--------".format(mode))
 
-
-        """with open(input_dir, 'r', encoding='utf-8') as f:
-            word_list = []
-            label_list = []
-            num = 0
-            sep_num = 0
-            for line in f:
-                words = []
-                line = line.strip()  # remove spaces at the beginning and the end
-                if not line:
-                    continue  # line is None
-                for i in range(len(line)):
-                    if line[i] == " ":
-                        continue  # skip space
-                    words.append(line[i])
-                text = line.split(" ")
-                labels = []
-                for item in text:
-                    if item == "":
-                        continue
-                    labels.extend(getlist(item))
-                if len(words) > config.max_len:
-                    # 直接按最大长度切分
-                    sub_word_list = get_sub_list(words, config.max_len - 5, config.sep_word)
-                    sub_label_list = get_sub_list(labels, config.max_len - 5, config.sep_label)
-                    word_list.extend(sub_word_list)
-                    label_list.extend(sub_label_list)
-                    sep_num += 1
-                else:
-                    word_list.append(words)
-                    label_list.append(labels)
-                num += 1
-                assert len(labels) == len(words), "labels 数量与 words 不匹配"
-                assert len(word_list) == len(label_list), "label 句子数量与 word 句子数量不匹配"
-            print("We have", num, "lines in", mode, "file processed")
-            print("We have", sep_num, "lines in", mode, "file get sep processed")
-            # 保存成二进制文件
-            np.savez_compressed(output_dir, words=word_list, labels=label_list)
-            logging.info("-------- {} data process DONE!--------".format(mode))"""
+def get_sub_list(init_list, sublist_len, sep_word):
+    """直接按最大长度切分"""
+    list_groups = zip(*(iter(init_list),) * sublist_len)
+    end_list = [list(i) + list(sep_word) for i in list_groups]
+    count = len(init_list) % sublist_len
+    if count != 0:
+        end_list.append(init_list[-count:])
+    else:
+        end_list[-1] = end_list[-1][:-1]  # remove the last sep word
+    return end_list
 
 
 def get_process():
